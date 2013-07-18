ed1f1a5f57291a5d502c3ee23e46fc794e68f8b5
Dominic Marcello <dmarce1@lsu.edu>
2013-07-18 12:07:03 -0500

    Changed copyright notice

--- src/main.c
+++ src/main.c
@@ -38,8 +38,20 @@ int main( int argc, char* argv[] )
 #ifdef BOOK_TOOL
 	printf( "Book Tool Utility\n" );
 #endif
-	printf( "Copyright (C) 2007,2008 Dominic C. Marcello\n" );
-	printf( "ALL RIGHTS RESERVED\n" );
+printf( "MorphyChess 1.0.5 Copyright (C) 2008 Dominic C. Marcello   (dmarcello@phys.lsu.edu)\n" );
+printf( "\n" );
+printf( "    This program is free software: you can redistribute it and/or modify\n" );
+printf( "    it under the terms of the GNU General Public License as published by\n" );
+printf( "    the Free Software Foundation, either version 3 of the License, or\n" );
+printf( "    (at your option) any later version.\n" );
+printf( "\n" );
+printf( "    This program is distributed in the hope that it will be useful,\n" );
+printf( "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n" );
+printf( "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" );
+printf( "    GNU General Public License for more details.\n" );
+printf( "\n" );
+printf( "    You should have received a copy of the GNU General Public License\n" );
+printf( "    along with this program.  If not, see [http://www.gnu.org/licenses/].\n" );
 	printf( "\n" );
 	if( argc > 1 )
 	{
